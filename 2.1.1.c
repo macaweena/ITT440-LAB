aa9bdee450e8c18d9ccf0a6ec15dd38dd0ea48ec
macaweena <illegirl666@gmail.com>
2022-04-28 13:20:12 +0800

    2.1.1.c adding comment

--- 2.1.1.c
+++ 2.1.1.c
@@ -1,11 +1,11 @@
 
 #include <stdio.h>
 #include <sys/types.h>
-#include <unistd.h>
+/*#include <unistd.h>*/
 
 int main(){
 fork();
 printf("Hello World :D  \n");
 return 0;
 }
-
+/*erasing unistd.h*/
